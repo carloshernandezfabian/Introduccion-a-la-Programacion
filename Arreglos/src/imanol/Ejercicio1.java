c919a663f02e50dd8f945947955dd2cd15c3e6d1
imanol <DiegoSvertti@gmail.com>
2019-04-24 11:36:21 -0500

    falta terminar y revisar

--- Arreglos/src/imanol/Ejercicio1.java
+++ Arreglos/src/imanol/Ejercicio1.java
@@ -1,6 +1,27 @@
-package imanol;
+package imanol;// // incompleto por fallas con el equipo
+import java.util.Scanner;    // por terminar
+
+
+//  1) Solicitar por teclado la dimensión del arreglo, 
+//2) agregar numeros a cada posición del arreglo
+//3) mostrar los números del arreglo 
+//4) despues mostrar la suma de los valores del arreglo.
+
 
 public class Ejercicio1 {
-
+	public static void main (String[] args);
+		
+		System.out.println("inserta el valor de arreglo en x");
+		int Arreglox = entrada.nextInt();
+		
+		int entrada = Arreglox;
+		int[] arregloNumeros = new int[Arreglox];
+		
+		System.out.println("Mostrando valores del arreglo");
+		for (int Arreglox = 0; indice < arregloNumeros.length; indice++);
+	
+		
+		
+		
 }
-// prueba diagostico
+
