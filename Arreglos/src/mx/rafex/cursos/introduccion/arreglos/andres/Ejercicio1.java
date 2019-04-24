a3f5ec4531f003cc16259f34bafa41b2a7719648
Raúl González <rafex.dev@gmail.com>
2019-04-24 10:40:29 -0500

    Por lo menos ya suma

--- Arreglos/src/mx/rafex/cursos/introduccion/arreglos/andres/Ejercicio1.java
+++ Arreglos/src/mx/rafex/cursos/introduccion/arreglos/andres/Ejercicio1.java
@@ -1,17 +1,21 @@
 package mx.rafex.cursos.introduccion.arreglos.andres;
 
 public class Ejercicio1 {
-	public static void main(String[] args){
-	final int []  arregloNumeros = new int [4];
-	i = 0;
-			arregloNumeros[0] = 1;
-			arregloNumeros[1] = 2;
-			arregloNumeros[2] = 3;
-			arregloNumeros[3] = 4;
-	
-			System.out.println(arregloNumeros.lenght);
-			 for (int x = 0; x < arregloNumeros.length; x++) {
-			 System.out.println("los numeros dentro son: " + x);
-			 }
-	}
+    public static void main(String[] args) {
+
+        final int[] arregloNumeros = new int[4];
+
+        arregloNumeros[0] = 1;
+        arregloNumeros[1] = 2;
+        arregloNumeros[2] = 3;
+        arregloNumeros[3] = 4;
+
+        int sumaArreglo = 0;
+        System.out.println(arregloNumeros.length);
+        for (final int arregloNumero : arregloNumeros)
+            sumaArreglo += arregloNumero;
+
+        System.out.println("La suma es de: " + sumaArreglo);
+
+    }
 }
