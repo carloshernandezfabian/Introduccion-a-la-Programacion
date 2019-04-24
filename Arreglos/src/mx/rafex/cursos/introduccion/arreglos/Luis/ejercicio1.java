b6a7ca825de949ceaceb4d9ec9c7e7a6980c5695
Luis Angeles <luisangeltkd@hotmail.com>
2019-04-24 10:08:47 -0500

    La lucha se hace

--- Arreglos/src/mx/rafex/cursos/introduccion/arreglos/Luis/ejercicio1.java
+++ Arreglos/src/mx/rafex/cursos/introduccion/arreglos/Luis/ejercicio1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  */
 public class EjercicioUno {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
         try (final Scanner entrada = new Scanner(System.in)) {
 
